54991dc24119b628c629e0b7717ef6b5cde65f21
Adria <adriafurze@gmail.com>
2017-09-14 15:48:03 +1200

    Testing map stuff

--- js/script.js
+++ js/script.js
@@ -266,3 +266,43 @@ var getAccountBank = fetch(baseAPI + 'account/bank' + accessToken + currentKey)
 				}
 		});
 });
+
+
+var getMapIds = fetch(baseAPI + 'maps')
+	.then(function(response){
+		return response.json();
+	})
+
+getMapIds.then(function(response){
+
+				let getMapInfo1 = fetch(baseAPI + 'maps?ids=' + response.slice(0, pageLimit))
+				.then(function(response){
+					return response.json();
+				})
+
+				let getMapInfo2 = fetch(baseAPI + 'maps?ids=' + response.slice(200, 400))
+				.then(function(response){
+					return response.json();
+				})
+
+				let getMapInfo3 = fetch(baseAPI + 'maps?ids=' + response.slice(400, 600))
+				.then(function(response){
+					return response.json();
+				})
+
+				let getMapInfo4 = fetch(baseAPI + 'maps?ids=' + response.slice(600, 800))
+				.then(function(response){
+					return response.json();
+				})
+
+				Promise.all([getMapInfo1, getMapInfo2, getMapInfo3, getMapInfo4]).then(values => {
+									totalMapList = values[0].concat(values[1], values[2], values[3])
+									console.log(totalMapList);
+
+								var publicMaps =  totalMapList.filter(function (el) {
+									return el.type == "Public"
+								})
+
+								console.log(publicMaps)
+				})
+})
