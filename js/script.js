324813a3715427cd8e4c1fc0bf030a5832157909
Adrian peter Furze <845865197@cit3-l5-05.auckland.natcoll.ac.nz>
2017-09-21 11:56:27 +1200

    fixed

--- js/script.js
+++ js/script.js
@@ -274,40 +274,6 @@ var getMapIds = fetch(baseAPI + 'maps')
 	})
 
 getMapIds.then(function(response){
-<<<<<<< HEAD
 								var myWorker = new Worker('js/test.js');
 								myWorker.postMessage([response, baseAPI]);
-=======
-
-				let getMapInfo1 = fetch(baseAPI + 'maps?ids=' + response.slice(0, pageLimit))
-				.then(function(response){
-					return response.json();
-				})
-
-				let getMapInfo2 = fetch(baseAPI + 'maps?ids=' + response.slice(200, 400))
-				.then(function(response){
-					return response.json();
-				})
-
-				let getMapInfo3 = fetch(baseAPI + 'maps?ids=' + response.slice(400, 600))
-				.then(function(response){
-					return response.json();
-				})
-
-				let getMapInfo4 = fetch(baseAPI + 'maps?ids=' + response.slice(600, 800))
-				.then(function(response){
-					return response.json();
-				})
-
-				Promise.all([getMapInfo1, getMapInfo2, getMapInfo3, getMapInfo4]).then(values => {
-									totalMapList = values[0].concat(values[1], values[2], values[3])
-									console.log(totalMapList);
-
-								var publicMaps =  totalMapList.filter(function (el) {
-									return el.type == "Public"
-								})
-
-								console.log(publicMaps)
-				})
->>>>>>> 54991dc24119b628c629e0b7717ef6b5cde65f21
 })
